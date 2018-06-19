490dc7da69e67dbb652c41b88e1e782e36f61005
David Hopkins <dlhopkins2@gmail.com>
2018-06-19 23:03:58 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,10 +3,7 @@ class Genre
 
   @@all = []
 
-  def initialize(name, artist, genre)
-    @name << name
-    @artist << artist
-    @genre << genre
+  def initialize(name)
     @@all << self
   end
 
